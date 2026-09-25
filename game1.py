722de3637238ffd92ca31dd928eb844cbd4694bd
Катя <makarik.08@inbox.ru>
2026-09-25 22:31:47 +0300

    Initial commit

---
+++ game1.py
@@ -0,0 +1,22 @@
+import random
+
+
+def get_user_choice():
+    pass
+
+
+def get_computer_choice():
+    choices = ['камень', 'ножницы', 'бумага']
+    return random.choice(choices)
+
+
+def determine_winner(user_choice, computer_choice):
+    pass
+
+
+def main():
+    print("Добро пожаловать в игру 'Камень, Ножницы, Бумага'!")
+
+
+if __name__ == "__main__":
+    main()
